6a2b504020a9b813761e26667b2c87fa87539cb1
thomashessel <thomas.silva482@gmail.com>
2022-08-28 19:52:50 -0300

    Novo projeto treino aulao 3 e 4 prof nelio

---
+++ projpedidos/src/main/java/com/treinoweekspringreact/ProjpedidosApplication.java
@@ -0,0 +1,13 @@
+package com.treinoweekspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjpedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjpedidosApplication.class, args);
+	}
+
+}
---
+++ projpedidos/src/test/java/com/treinoweekspringreact/ProjpedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinoweekspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjpedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
